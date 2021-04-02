820e45bccdb6b45bbe3a269776db7da6a1876899
Ryan Alamo <ralamo@pop-os.localdomain>
2021-04-02 18:47:12 -0400

    Checking classroom

--- frontend/src/components/world/WorldMap.tsx
+++ frontend/src/components/world/WorldMap.tsx
@@ -500,7 +500,7 @@ export default function WorldMap(): JSX.Element {
 
     const game = new Phaser.Game(config);
     if (video) {
-      const newGameScene = new CoveyGameScene(MapSelection.Standard, video, emitMovement);
+      const newGameScene = new CoveyGameScene(MapSelection.Classroom, video, emitMovement);
       setGameScene(newGameScene);
       game.scene.add('coveyBoard', newGameScene, true);
       video.pauseGame = () => {
